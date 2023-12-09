9af3ba6e895f2b07fbf0fa95ad63742642a8a29a
Selfsigned <me@selfsigned.dev>
2023-12-09 03:05:00 +0100

    Fonts, Theming, Hero basic layout, Static adapter setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      bitmap: ['DotGothic16'],
+      sans: ['Space Grotesk'],
+      mono: ['Space Mono'],
+    },
   },
   plugins: [
     require("daisyui"),
   ],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#33ff00",
+          "base-content": "#ffffff",
+        },
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#33ff00",
+        },
+      },
+    ],
+  }
 }
-
