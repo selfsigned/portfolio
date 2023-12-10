140d5ec266d59aa285c6bde6574b916e9b657419
Selfsigned <me@selfsigned.dev>
2023-12-10 17:50:11 +0100

    Add prettier w/ tailwind integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,25 +13,25 @@
 <!-- hero -->
 <section>
 	<!-- CTA / Contact me -->
-	<div class="fixed top-5 right-10">
+	<div class="fixed right-10 top-5">
 		{#if looking_for_work}
 			<a
-				class="btn font-bitmap text-md md:text-lg"
+				class="text-md btn font-bitmap md:text-lg"
 				href="mailto:me@selfigned.dev?subject=Super Cool Job Opportunity&body=Hi Xavier!"
 			>
 				{contact_text}
 			</a>
 		{:else}
-			<a class="btn font-bitmap text-md md:text-lg" href="mailto:me@selfigned.dev?body=Hi Xavier!">
+			<a class="text-md btn font-bitmap md:text-lg" href="mailto:me@selfigned.dev?body=Hi Xavier!">
 				{contact_text}
 			</a>
 		{/if}
 	</div>
 	<!-- Content -->
-	<div class="flex flex-row h-screen">
+	<div class="flex h-screen flex-row">
 		{#if looking_for_work}
 			<!-- Open to work banner -->
-			<div class="[writing-mode:vertical-lr] rotate-180 whitespace-nowrap justify-start">
+			<div class="rotate-180 justify-start whitespace-nowrap [writing-mode:vertical-lr]">
 				<!-- TODO infinite marquee -->
 				<p class="font-bitmap text-xl md:text-3xl">
 					Open to Work! Let's get in touch | Open to Work! Let's get in touch | Open to Work! Let's
@@ -39,14 +39,14 @@
 				</p>
 			</div>
 		{/if}
-		<div class="grow grid grid-cols-1 md:grid-cols-2">
+		<div class="grid grow grid-cols-1 md:grid-cols-2">
 			<!-- About me -->
-			<div class="h-full flex flex-col items-center justify-center">
+			<div class="flex h-full flex-col items-center justify-center">
 				<!-- Name / Job -->
 				<div>
-					<p class="text-2xl my-1 md:my-5 md:text-4xl lg:text-7xl">
+					<p class="my-1 text-2xl md:my-5 md:text-4xl lg:text-7xl">
 						HI, I'M<br />
-						<span class="font-bitmap text-primary tracking-widest text:2xl md:text-5xl lg:text-8xl">
+						<span class="text:2xl font-bitmap tracking-widest text-primary md:text-5xl lg:text-8xl">
 							XAVIER<br />PERRIN<br />
 						</span>
 						<!-- TODO typing animation -->
@@ -58,7 +58,7 @@
 					<!-- github -->
 					<a class="mr-2" href="https://github.com/selfsigned">
 						<svg
-							class="fill-base-content hover:fill-neutral-content mr-15 w-5 md:mr-30 md:w-10"
+							class="mr-15 md:mr-30 w-5 fill-base-content hover:fill-neutral-content md:w-10"
 							viewBox="0 0 100 100 "
 						>
 							<Githubsvg></Githubsvg>
@@ -67,7 +67,7 @@
 					<!-- linkedin -->
 					<a class="mr-2" href="https://linkedin.com/in/selfsigned">
 						<svg
-							class="fill-base-content hover:fill-neutral-content mr-15 w-5 md:mr-30 md:w-10"
+							class="mr-15 md:mr-30 w-5 fill-base-content hover:fill-neutral-content md:w-10"
 							viewBox="0 0 24 24"
 						>
 							<Linkedinsvg></Linkedinsvg>
