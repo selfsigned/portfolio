4d75655c8356332a50dc3dc481feda9fa3aee13b
Selfsigned <me@selfsigned.dev>
2023-12-14 05:27:08 +0100

    Typewritter effect

---
+++ src/lib/animations/typing.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Typewriter from 'typewriter-effect/dist/core';
+
+	export let text: Array<String> = [];
+	export let cursor: boolean = true;
+
+	let text_elem: HTMLElement | null;
+
+	onMount(() => {
+		text_elem = document?.getElementById('tgt-text');
+		if (text_elem != null) {
+			new Typewriter('#tgt-text', {
+				strings: text,
+				pauseFor: 1800,
+				autoStart: true,
+				loop: true,
+				cursor: cursor ? '_' : ''
+			});
+		}
+	});
+</script>
+
+<span id="tgt-text"></span>
