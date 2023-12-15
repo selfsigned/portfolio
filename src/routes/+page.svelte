2a1ec14b8ea5fb0d4be28dcc5b59dd06c5a063f5
Selfsigned <me@selfsigned.dev>
2023-12-15 06:02:46 +0100

    Gradiant + Improved mobile exp (contact btn, fonts)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,22 +12,7 @@
 </svelte:head>
 
 <!-- hero -->
-<section class="overflow-hidden">
-	<!-- CTA / Contact me -->
-	<div class="fixed right-10 top-5">
-		{#if looking_for_work}
-			<a
-				class="text-md btn font-bitmap md:text-lg"
-				href="mailto:me@selfigned.dev?subject=Super Cool Job Opportunity&body=Hi Xavier!"
-			>
-				{contact_text}
-			</a>
-		{:else}
-			<a class="text-md btn font-bitmap md:text-lg" href="mailto:me@selfigned.dev?body=Hi Xavier!">
-				{contact_text}
-			</a>
-		{/if}
-	</div>
+<div class="via-base--200 overflow-hidden bg-gradient-to-br from-base-100 from-65% to-base-300">
 	<!-- Content -->
 	<div class="flex h-screen flex-row">
 		{#if looking_for_work}
@@ -44,15 +29,15 @@
 		<div class="grid grow grid-cols-1 md:grid-cols-2">
 			<!-- About me -->
 			<div
-				class="my-1 flex h-full flex-col items-center justify-center text-2xl
-				md:my-5 md:text-4xl lg:text-7xl"
+				class="my-1 flex h-full flex-col items-center justify-center text-4xl
+				md:my-5 lg:text-7xl"
 			>
 				<!-- Name / Job -->
 				<div>
 					<p>
 						HI, I'M<br />
 						<span
-							class="text:2xl font-bitmap tracking-widest text-primary shadow-primary text-shadow-md md:text-5xl lg:text-8xl"
+							class="font-bitmap tracking-widest text-primary shadow-primary text-shadow-md md:text-5xl lg:text-8xl"
 						>
 							XAVIER<br />PERRIN<br />
 						</span>
@@ -64,11 +49,11 @@
 						<TypingAnim selector="#title-anim" text={['DevOps']} loop={false} />
 					</div>
 					<!-- Socials -->
-					<div class="mr-20 flex justify-center space-x-2 md:mr-0">
+					<div class="mr-10 flex justify-center space-x-2 md:mr-0">
 						<!-- github -->
 						<a target="_blank" rel="noopener" href="https://github.com/selfsigned">
 							<svg
-								class="w-5 fill-base-content hover:fill-neutral-content md:w-7 lg:w-10"
+								class="w-7 fill-base-content hover:fill-neutral-content lg:w-10"
 								viewBox="0 0 100 100 "
 							>
 								<Githubsvg></Githubsvg>
@@ -77,7 +62,7 @@
 						<!-- linkedin -->
 						<a target="_blank" rel="noopener" href="https://linkedin.com/in/selfsigned">
 							<svg
-								class="w-5 fill-base-content hover:fill-neutral-content md:w-7 lg:w-10"
+								class="w-7 fill-base-content hover:fill-neutral-content lg:w-10"
 								viewBox="0 0 24 24"
 							>
 								<Linkedinsvg></Linkedinsvg>
@@ -96,4 +81,19 @@
 			</div>
 		</div>
 	</div>
-</section>
+	<!-- CTA / Contact me -->
+	<div class="fixed right-10 top-5">
+		{#if looking_for_work}
+			<a
+				class="text-md btn font-bitmap md:text-lg"
+				href="mailto:me@selfigned.dev?subject=Super Cool Job Opportunity&body=Hi Xavier!"
+			>
+				{contact_text}
+			</a>
+		{:else}
+			<a class="text-md btn font-bitmap md:text-lg" href="mailto:me@selfigned.dev?body=Hi Xavier!">
+				{contact_text}
+			</a>
+		{/if}
+	</div>
+</div>
