7aca8cb92c2e7bf9272003e6846aa9385ea8e9d3
Selfsigned <me@selfsigned.dev>
2024-02-12 18:33:06 +0100

    Working contact lambda, checkout action bump

--- infra/contact.py
+++ infra/contact.py
@@ -1,8 +1,58 @@
 #!env python3
+import re
 import json
+import boto3
+
+client = boto3.client('ses', region_name='eu-north-1')
+
+def respond(status_code, msg):
+    return {
+        'statusCode': status_code,
+        'body': msg
+    }
+
+def send_mail(name, email, message):
+    subject = f"Selfsigned.dev Contact - {email}"
+    text = f"Name: {name}\nEmail: {email}\n Message:\n{message}"
+    my_email = 'me@selfsigned.dev'
+
+    client.send_email(
+    Destination={
+        'ToAddresses': [my_email]
+    },
+    Message={
+        'Body': {
+            'Text': {
+                'Charset': 'UTF-8',
+                'Data': text
+            }
+        },
+        'Subject': {
+            'Charset': 'UTF-8',
+            'Data': subject
+            }
+        },
+        Source=my_email)
+
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('hello world!')
-    }
+
+    try:
+      # validate request
+      body = json.loads(event['body'])
+
+      name = body.get('name')
+      email = body.get('email')
+      message = body.get('message')
+
+      if not name:
+        return respond(400, 'Name required')
+      if not (email and re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email)):
+        return respond(400, 'Valid Email required') 
+      if not message:
+        return respond(400, 'Message required')
+    except Exception as e:
+        return respond(500, type(e).__name__)
+    
+    send_mail(name, email, message)
+    return respond(200, 'Email sent')
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,4 +1,5 @@
-data "aws_iam_policy_document" "lambda_contact_role" {
+// base IAM policy / role
+data "aws_iam_policy_document" "lambda_role" {
   statement {
     effect = "Allow"
 
@@ -11,13 +12,34 @@ data "aws_iam_policy_document" "lambda_contact_role" {
   }
 }
 
+
 resource "aws_iam_role" "iam_contact_lambda" {
   name               = "${var.domain}-lambda-contact"
-  assume_role_policy = data.aws_iam_policy_document.lambda_contact_role.json
+  assume_role_policy = data.aws_iam_policy_document.lambda_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
+resource "aws_iam_role_policy_attachment" "contact_lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.iam_contact_lambda.name
+}
+
+// Sendmail policy attached to role
+data "aws_iam_policy_document" "sendmail_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["ses:SendEmail", "ses:SendRawEmail"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "sendmail_policy" {
+  name        = "sendmail_policy"
+  description = "Policy for SES sendMail and SendRawEmail"
+  policy      = data.aws_iam_policy_document.sendmail_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "contact_lambda_sendmail" {
+  policy_arn = aws_iam_policy.sendmail_policy.arn
   role       = aws_iam_role.iam_contact_lambda.name
 }
 
