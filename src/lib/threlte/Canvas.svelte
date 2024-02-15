d086615b60f4a7758fc65d05d8ca1d905f2e6e2b
Xavier Perrin <me@selfsigned.dev>
2024-02-16 00:17:01 +0100

    Animate camera (#6)
    * Fix dark theme
    
    * Updated README with the stack
    
    * Working scroll detection
    
    * Working scroll -> theatre binding
    
    * Fixed build and theatre prod mode

--- src/lib/threlte/Canvas.svelte
+++ src/lib/threlte/Canvas.svelte
@@ -2,11 +2,15 @@
 	import { Canvas } from '@threlte/core';
 	import Scene from '$lib/threlte/Scene.svelte';
 
-	// dbg
+	// dev
+	import { dev } from '$app/environment';
+	import { Studio } from '@threlte/theatre';
 	// import { Inspector } from 'three-inspect';
 
 	let scene;
 </script>
+
+<Studio enabled={dev} />
 
 <Canvas useLegacyLights={false}>
 	<Scene bind:this={scene} />
--- src/lib/threlte/Scene.svelte
+++ src/lib/threlte/Scene.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { interactivity, Gizmo, OrbitControls } from '@threlte/extras';
+	import { interactivity, Text, Gizmo, OrbitControls } from '@threlte/extras';
 	interactivity();
 
+	import { Scroll } from '$lib/stores/view.ts';
+	import { SheetObject, Project, Sheet, Sequence } from '@threlte/theatre';
 	import Laptop from './Laptop.svelte';
-	import { BoxGeometry } from 'three';
+
+	// Theatre
+	import threlteProjectConfig from './threlte.theatre-project-state.json';
+
+	$: posToProj = $Scroll.index == 0 ? Math.max(($Scroll.offset - 0.5) * 2, 0) : 1;
 </script>
 
-<T.PerspectiveCamera makeDefault position={[0, 14, 30]} rotation={[-0.2, 0, 0]}>
-	<!-- <OrbitControls enableDamping maxPolarAngle={1.5} /> -->
-</T.PerspectiveCamera>
-
-<Gizmo></Gizmo>
+<Project name="threlte" config={{ state: threlteProjectConfig }}>
+	<Sheet name="hero_to_proj">
+		<Sequence position={posToProj}>
+			<SheetObject key="camera" let:Transform>
+				<Transform>
+					<T.PerspectiveCamera makeDefault position={[0, 4, 30]} rotation={[-0.2, 0, 0]} />
+				</Transform>
+			</SheetObject>
+		</Sequence>
+	</Sheet>
+</Project>
+<Gizmo />
 
 <T.DirectionalLight position={[0, 10, 10]} intensity={20} castShadow />
 
-<!-- <T.GridHelper args={[200, 20]} /> -->
 <T.GridHelper args={[400, 40]} />
+
+<!-- Scroll indicator -->
+<Text
+	name="scroll indicator"
+	fontSize={20}
+	position={[-80, 60, -400]}
+	text={`Progress: ${$Scroll.progress}\noffset: ${$Scroll.offset}\nindex: ${$Scroll.index}`}
+/>
 
 <Laptop />
