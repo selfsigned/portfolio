df97ae7db7a418b291d600f8b6147020c8fcf8e9
Xavier Perrin <me@selfsigned.dev>
2024-02-29 16:28:46 +0100

    Resume (#10)

---
+++ src/lib/resume/SidebarSection.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let title: string;
+	export let titleClass: string = '';
+</script>
+
+<h3 class="font-vcr text-lg {titleClass}">{title}</h3>
+<div class="divider mx-[38%] mb-2 mt-1 h-0" />
+<div class="mb-6 ml-4 self-start text-sm">
+	<slot />
+</div>
